aa9bfee0ee7108e0894db3c8e950a3bfe72dff04
CrossStackQ <crossstack@gmail.com>
2022-12-28 16:29:12 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,89 @@ import * as fs from "fs"
 // now furthere ok
 
 
+
+async function createTokenMetadata(
+  connection: web3.Connection,
+  metaplex: Metaplex,
+  mint: web3.PublicKey,
+  user: web3.Keypair,
+  name: string,
+  symbol: string,
+  description: string
+) {
+  // file to buffer
+  const buffer = fs.readFileSync("assets/pizza.png")
+
+  // buffer to metaplex file
+  const file = toMetaplexFile(buffer, "pizza.png")
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+  // upload metadata and get metadata uri (off chain metadata)
+  const { uri } = await metaplex
+    .nfts()
+    .uploadMetadata({
+      name: name,
+      description: description,
+      image: imageUri,
+    })
+ 
+  console.log("metadata uri:", uri)
+
+  // get metadata account address
+  const metadataPDA = metaplex.nfts().pdas().metadata({mint})
+
+  // onchain metadata format
+  const tokenMetadata = {
+    name: name,
+    symbol: symbol,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  } as DataV2
+
+  // transaction to create metadata account
+  const transaction = new web3.Transaction().add(
+    createCreateMetadataAccountV2Instruction(
+      {
+        metadata: metadataPDA,
+        mint: mint,
+        mintAuthority: user.publicKey,
+        payer: user.publicKey,
+        updateAuthority: user.publicKey,
+      },
+      {
+        createMetadataAccountArgsV2: {
+          data: tokenMetadata,
+          isMutable: true,
+        },
+      }
+    )
+  )
+
+  // send transaction
+  const transactionSignature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [user]
+  )
+
+  console.log(
+    `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
+
+
+
+
+// above is function token Metadata
+
+
+
 async function createNewMint(
   connection: web3.Connection,
   payer: web3.Keypair,
