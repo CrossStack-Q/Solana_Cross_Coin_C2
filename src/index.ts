eb237c07e4f71660b1757132981a98bd204dd87a
CrossStackQ <crossstack@gmail.com>
2022-12-28 15:51:39 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,134 +1,28 @@
 import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
-
-
-
-import {
-  Metaplex,
-  keypairIdentity,
-  bundlrStorage,
-  toMetaplexFile,
-} from "@metaplex-foundation/js"
-import {
-  DataV2,
-  createCreateMetadataAccountV2Instruction,
-  createUpdateMetadataAccountV2Instruction,
-} from "@metaplex-foundation/mpl-token-metadata"
-import * as fs from "fs"
-
-
 import * as token from "@solana/spl-token"
 
-
-
-
-
-// Main wala function of Metaplex
-
-async function createTokenMetadata(
+async function createNewMint(
   connection: web3.Connection,
-  metaplex: Metaplex,
-  mint: web3.PublicKey,
-  user: web3.Keypair,
-  name: string,
-  symbol: string,
-  description: string
+  payer: web3.Keypair,
+  mintAuthority: web3.PublicKey,
+  freezeAuthority: web3.PublicKey | null,
+  decimals: number
 ) {
-  // file to buffer
-  const buffer = fs.readFileSync("assets/pizza.png")
-
-  // buffer to metaplex file
-  const file = toMetaplexFile(buffer, "pizza.png")
-
-  // upload image and get image uri
-  const imageUri = await metaplex.storage().upload(file)
-  console.log("image uri:", imageUri)
-
-  // upload metadata and get metadata uri (off chain metadata)
-  const { uri } = await metaplex
-    .nfts()
-    .uploadMetadata({
-      name: name,
-      description: description,
-      image: imageUri,
-    })
- 
-  console.log("metadata uri:", uri)
-
-  // get metadata account address
-  const metadataPDA = metaplex.nfts().pdas().metadata({mint})
-
-  // onchain metadata format
-  const tokenMetadata = {
-    name: name,
-    symbol: symbol,
-    uri: uri,
-    sellerFeeBasisPoints: 0,
-    creators: null,
-    collection: null,
-    uses: null,
-  } as DataV2
-
-  // transaction to create metadata account
-  const transaction = new web3.Transaction().add(
-    createCreateMetadataAccountV2Instruction(
-      {
-        metadata: metadataPDA,
-        el
-        mint: mint,
-        mintAuthority: user.publicKey,
-        payer: user.publicKey,
-        updateAuthority: user.publicKey,
-      },
-      {
-        createMetadataAccountArgsV2: {
-          data: tokenMetadata,
-          isMutable: true,
-        },
-      }
-    )
-  )
-
-  // send transaction
-  const transactionSignature = await web3.sendAndConfirmTransaction(
+  const tokenMint = await token.createMint(
     connection,
-    transaction,
-    [user]
+    payer,
+    mintAuthority,
+    freezeAuthority,
+    decimals
   )
 
   console.log(
-    `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
   )
+
+  return tokenMint
 }
-
-
-async function createNewMint(
-    connection: web3.Connection,
-    payer: web3.Keypair,
-    mintAuthority: web3.PublicKey,
-    freezeAuthority: web3.PublicKey,
-    decimals: number
-): Promise<web3.PublicKey> {
-
-    const tokenMint = await token.createMint(
-        connection,
-        payer,
-        mintAuthority,
-        freezeAuthority,
-        decimals
-    );
-
-    console.log(`The token mint account address is ${tokenMint}`)
-    console.log(
-        `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
-    );
-
-    return tokenMint;
-}
-
-
-
-
 
 async function createTokenAccount(
   connection: web3.Connection,
@@ -137,19 +31,18 @@ async function createTokenAccount(
   owner: web3.PublicKey
 ) {
   const tokenAccount = await token.getOrCreateAssociatedTokenAccount(
-      connection,
-      payer,
-      mint,
-      owner
+    connection,
+    payer,
+    mint,
+    owner
   )
-  
+
   console.log(
-      `Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
+    `Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
   )
 
   return tokenAccount
 }
-
 
 async function mintTokens(
   connection: web3.Connection,
@@ -174,8 +67,6 @@ async function mintTokens(
     `Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
   )
 }
-
-
 
 async function transferTokens(
   connection: web3.Connection,
@@ -202,44 +93,6 @@ async function transferTokens(
   )
 }
 
-
-
-
-
-
-
-//  For Burning
-
-async function burnTokens(
-  connection: web3.Connection,
-  payer: web3.Keypair,
-  account: web3.PublicKey,
-  mint: web3.PublicKey,
-  owner: web3.Keypair,
-  amount: number
-) {
-
-  const mintInfo = await token.getMint(connection, mint)
-  
-  const transactionSignature = await token.burn(
-      connection,
-      payer,
-      account,
-      mint,
-      owner,
-      amount * 10 ** mintInfo.decimals
-  )
-
-  console.log(
-      `Burn Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
-  )
-}
-
-
-
-
-
-
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
   const user = await initializeKeypair(connection)
@@ -248,50 +101,38 @@ async function main() {
 
   const mint = await createNewMint(
     connection,
-    user,           // We'll pay the fees
-    user.publicKey, // We're the mint authority
-    user.publicKey, // And the freeze authority >:)
-    2               // Only two decimals!
+    user,
+    user.publicKey,
+    user.publicKey,
+    2
   )
 
   const tokenAccount = await createTokenAccount(
-    connection,     
-    user,           
-    mint,            
-    user.publicKey   // Associating our address with the token account
+    connection,
+    user,
+    mint,
+    user.publicKey
   )
-  
-  // Mint 100 tokens to our address
+
   await mintTokens(connection, user, mint, tokenAccount.address, user, 100)
 
+  const recipientTokenAccount = await token.getOrCreateAssociatedTokenAccount(
+    connection,
+    user,
+    mint,
+    new web3.PublicKey("BpnBxp5KvnupqYVutjYwyhmQi7wQrU5xZXXGRgZcKDSj")
+  )
 
-
-//  Givibg tokens to all of them ,,.
-
-const receiver = web3.Keypair.generate().publicKey
-    
-    const receiverTokenAccount = await createTokenAccount(
-        connection,
-        user,
-        mint,
-        receiver
-    )
-
-    await transferTokens(
-        connection,
-        user,
-        tokenAccount.address,
-        receiverTokenAccount.address,
-        user.publicKey,
-        50,
-        mint
-    )
-    
-   await burnTokens(connection, user, tokenAccount.address, mint, user, 25)
-
-
+  await transferTokens(
+    connection,
+    user,
+    tokenAccount.address,
+    recipientTokenAccount.address,
+    user.publicKey,
+    50,
+    mint
+  )
 }
-
 
 main()
   .then(() => {
